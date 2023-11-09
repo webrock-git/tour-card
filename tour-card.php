e1e8606d5891d49f9b0fd56973e226aa335804c6
Mijanur Rahman <mahmudmizan717@gmail.com>
2023-11-09 20:59:56 +0600

    Complete

--- tour-card.php
+++ tour-card.php
@@ -6,7 +6,7 @@
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            mizan42047
+ * Author:            Hemanth Sharma
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       cover-video
@@ -22,5 +22,6 @@ if (!defined('ABSPATH')) {
 function tour_card_block_init()
 {
 	register_block_type(__DIR__ . '/build/block');
+	wp_register_style("google-fonts", "https://fonts.googleapis.com/css2?family=Figtree:wght@400;500;600&family=Plus+Jakarta+Sans:wght@400;700&display=swap", array(), "1.0.0", "all");
 }
 add_action('init', 'tour_card_block_init');
